b320c2c93fc1cbff1281733a4d1597f13fe97b5c
Alifuatakcn <fakcan4908@gmail.com>
2023-09-20 21:32:24 +0300

    switch

---
+++ day-05/js/script-27.js
@@ -0,0 +1,8 @@
+function showSalary(){
+    const txtSalary = document.querySelector("#txtSalary");
+    const elResult = document.querySelector("#result");
+    const elStatus = document.querySelector("input[name='status']:checked");
+
+    console.log(elStatus);
+    
+}
