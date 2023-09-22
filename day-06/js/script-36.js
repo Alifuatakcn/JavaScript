fb2b9f881808ea6fbccc05cdfa55129002cb0e5c
Alifuatakcn <fakcan4908@gmail.com>
2023-09-22 19:55:22 +0300

    Function-practice

---
+++ day-06/js/script-36.js
@@ -0,0 +1,15 @@
+
+const getLastPrice = (price, tax, discount) => {
+    price ??= 0; // price = price ?? 0;
+    tax ??= 0;
+    discount ??= 0;
+    if(discount <=0) tax = 0;
+    if(price>=1500) discount += 10;
+    const totalTax = price * tax / 100;
+    const totalDiscount = price * discount / 100;
+    const result = price  + totalTax - totalDiscount;
+    return result;
+}
+const onGetLastPriceClick = () => {
+    // Homework
+}
