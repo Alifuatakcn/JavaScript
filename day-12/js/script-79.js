08ed57094a339c2187bd49c368ac494adbb1572b
Alifuatakcn <fakcan4908@gmail.com>
2023-09-29 21:14:35 +0300

    timeout-practise

--- day-12/js/script-79.js
+++ day-12/js/script-79.js
@@ -1,6 +1,11 @@
 import { countries } from "../data/countries.js";
 
+let timeout = null;
+
 const filterData = (val) => {
+
+  if(!val) return [];
+
   const filteredData = countries.filter((country) =>
     country.name.common.toLowerCase().includes(val.toLowerCase())
   );
@@ -9,7 +14,27 @@ const filterData = (val) => {
 };
 
 
+const setResultList = (items) => { 
+  const results = document.getElementById("results");
+  results.innerHTML = "";
 
-document.getElementById("txtSearch").addEventListener("input", ()=>{
-    
+  items.forEach( item=> {
+    const li = document.createElement("li");
+    li.innerText = item.name.common;
+    results.appendChild(li);
+  });
+
+
+
+ }
+
+document.getElementById("txtSearch").addEventListener("input", (e)=>{
+    if(timeout) clearTimeout(timeout);
+
+    timeout = setTimeout( ()=> {
+      const filteredData = filterData(e.target.value);
+      setResultList(filteredData);
+
+      console.log(filteredData)
+    }, 500 )
 })
