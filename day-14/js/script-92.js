cd02c98e4c879fd50092ea07ba9b23ccb63a3086
Alifuatakcn <fakcan4908@gmail.com>
2023-10-04 20:18:23 +0300

    fetch

--- day-14/js/script-92.js
+++
@@ -1,9 +0,0 @@
-import showAlert, { showConfirm } from "./message.js";
-
-document.getElementById("btnAlert").addEventListener("click", () => {
-  showAlert("Hello module");
-});
-
-document.getElementById("btnConfirm").addEventListener("click", () => {
-  showConfirm("Hello module");
-});
