b22e1770346c263d90c4a384d3919ec72ee10054
Alifuatakcn <fakcan4908@gmail.com>
2023-10-05 20:39:19 +0300

    fetch practise

--- day-17/js/script-105.js
+++ day-17/js/script-105.js
@@ -1,9 +1,18 @@
 
 
 const getUsers = async () => { 
-    const resp = await fetch("https://651da7e644e393af2d5a2461.mockapi.io/api/v1/users")
-    const data = await resp.json();
-    return data;
+    try {
+        const resp = await fetch("https://651da7e644e393af2d5a2461.mockapi.io/api/v1/users")
+        const data = await resp.json();
+        return data;
+    
+    } catch (err) {
+        console.log(err);
+        return [];
+        
+    }
+
+
 }
 
 
@@ -13,16 +22,19 @@ const loadUserData =  async () => {
     let strHTML = "";
 
     users.forEach( (user)=>{
+        const { avatar, firstName, lastName } = user;
+
         strHTML += `<div class="col-md-3">
         <div class="card">
-          <img src="${user.avatar}" class="card-img-top" alt="..." />
+          <img src="${avatar}" class="card-img-top" alt="..." />
           <div class="card-body">
-            <h5 class="card-title">Card title</h5>
+            <h5 class="card-title">${firstName} ${lastName}</h5>
           </div>
         </div>
       </div>`
     })
-    document.getElementById("usersEl").innerHTML=strHTML ;
+
+    document.getElementById("usersEl").innerHTML = strHTML;
 }
 
 
